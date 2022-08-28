e752db23a74c2501d1e199a6f2330e1f02ec5b25
Alexandr Kuznetsov <47721260+AlexSKuznetsov@users.noreply.github.com>
2022-08-28 20:21:57 +0300

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/static';
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), react()],
+  site: 'https://alex-porfolio.vercel.app/',
+  integrations: [tailwind(), react(), sitemap()],
   adapter: vercel()
 });
