8b439d727d22481cef49af0efd3d88b16e4fba93
Alexandr Kuznetsov <47721260+AlexSKuznetsov@users.noreply.github.com>
2022-10-05 20:10:32 +0300

    update title, service and summary

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,13 +7,13 @@ export const Button = () => {
 
   return (
     <a
-      className='p-1 mx-3 inline-flex gap-y-2 gap-x-2 flex-wrap items-center text-neutral-50 border-slate-100/30 border rounded-full hover:border-opacity-50  hover:text-white cursor-pointer'
+      className='group p-1 mx-3 inline-flex gap-y-2 gap-x-2 flex-wrap items-center text-neutral-50 border-slate-100/30 border rounded-full hover:border-opacity-50  hover:text-white cursor-pointer'
       onClick={handleClick}
     >
       <span className='px-3 py-0.5 text-neutral-50 text-xs uppercase bg-blue-800 rounded-full'>
         Hire me
       </span>
-      <span className='text-xs hover:text-neutral-300'>
+      <span className='group-hover:static group-hover:opacity-100 transition duration-500 text-xs hover:text-neutral-300 absolute opacity-0'>
         Let's discuss future opportunities
       </span>
       <svg
@@ -22,7 +22,7 @@ export const Button = () => {
         viewBox='0 0 24 24'
         strokeWidth='1'
         stroke='currentColor'
-        className='w-5 h-5 pr-1'
+        className='w-5 h-5 pr-1 group-hover:static group-hover:opacity-100 transition duration-900 absolute opacity-0'
       >
         <path
           strokeLinecap='round'
