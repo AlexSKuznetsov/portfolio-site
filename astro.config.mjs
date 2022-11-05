5e8b82280b76a9d24a5663c288e0e9aaa0224ad4
Alexandr Kuznetsov <47721260+AlexSKuznetsov@users.noreply.github.com>
2022-11-05 22:36:53 +0200

    Added google analitics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/static';
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
@@ -10,5 +11,10 @@ export default defineConfig({
   output: 'static',
   adapter: vercel(),
   site: 'https://alex-porfolio.vercel.app/',
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [tailwind(), react(), sitemap(), partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })],
 });
