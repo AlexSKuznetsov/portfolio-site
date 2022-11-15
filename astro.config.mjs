73197a48843b26341e7e9dff7eeba95cc9899a34
Alexandr Kuznetsov <47721260+AlexSKuznetsov@users.noreply.github.com>
2022-11-15 19:59:25 +0200

    Add Image component, change vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/static';
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
   site: 'https://alex-porfolio.vercel.app/',
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [tailwind(), react(), sitemap(), image()]
 });
